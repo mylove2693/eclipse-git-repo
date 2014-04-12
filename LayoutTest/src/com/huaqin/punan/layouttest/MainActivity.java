ab16c86d90209c9f9d4b29a33d54d60474e0419d
punan <punan@huaqin.com>
2014-04-12 17:06:20 +0800

    modify

--- LayoutTest/src/com/huaqin/punan/layouttest/MainActivity.java
+++ LayoutTest/src/com/huaqin/punan/layouttest/MainActivity.java
@@ -17,9 +17,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
