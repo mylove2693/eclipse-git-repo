7cce48913d647c3269a4529e87b907f7e84e42b0
punan <punan2010@gmail.com>
2015-04-10 18:08:36 +0800

    new file

--- PaySetting/src/com/cynovo/paysetting/PaySettingActivity.java
+++ PaySetting/src/com/cynovo/paysetting/PaySettingActivity.java
@@ -1,41 +1,120 @@
 package com.cynovo.paysetting;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
 
-public class PaySettingActivity extends Activity {
+public class PaySettingActivity extends Activity implements
+		OnCheckedChangeListener {
+	
+	public static final String TAG = "PaySetting";
+	public static final String PREFERENCENAME = "payconfig";
+	public static final String PASSWORD = "123456";
 
+	public static final String KEY_YINLIAN = "yinlian";
+	public static final String KEY_ZHIFUBAO = "zhifubao";
+	public static final String KEY_WEIXIN = "weixin";
+	public static final String KEY_YIBAO = "yibao";
+	
+	private CheckBox cb_yinlian;
+	private CheckBox cb_zhifubao;
+	private CheckBox cb_weixin;
+	private CheckBox cb_yibao;
+	
 	private SharedPreferences PaySettingPreference;
 	private Editor editor;
-	
+
 	private PaySettingLogin LoginFragment;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pay_setting);
+
+		PaySettingPreference = PaySettingActivity.this.getSharedPreferences(
+				PREFERENCENAME, Context.MODE_PRIVATE);
+		editor = PaySettingPreference.edit();
+		
+		editor.putString(KEY_YINLIAN, "closed");
+		editor.putString(KEY_ZHIFUBAO, "closed");
+		editor.putString(KEY_WEIXIN, "closed");
+		editor.putString(KEY_YIBAO, "closed");
+
+		editor.commit();
+
+		cb_yinlian = (CheckBox) findViewById(R.id.cb_yinlian);
+		cb_zhifubao = (CheckBox) findViewById(R.id.cb_zhifubao);
+		cb_weixin = (CheckBox) findViewById(R.id.cb_weixin);
+		cb_yibao = (CheckBox) findViewById(R.id.cb_yibao);
+
+		cb_yinlian.setOnCheckedChangeListener(this);
+		cb_zhifubao.setOnCheckedChangeListener(this);
+		cb_weixin.setOnCheckedChangeListener(this);
+		cb_yibao.setOnCheckedChangeListener(this);
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.pay_setting, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		// TODO Auto-generated method stub
+		int id = buttonView.getId();
+		switch (id) {
+		case R.id.cb_yinlian:
+			try {
+				Toast.makeText(PaySettingActivity.this, "银联：" + isChecked,
+						Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				editor.putString(KEY_YINLIAN, str);
+				editor.commit();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case R.id.cb_zhifubao:
+			try {
+				Toast.makeText(PaySettingActivity.this, "支付宝：" + isChecked,
+						Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				editor.putString(KEY_ZHIFUBAO, str);
+				editor.commit();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case R.id.cb_weixin:
+			try {
+				Toast.makeText(PaySettingActivity.this, "微信：" + isChecked,
+						Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				editor.putString(KEY_WEIXIN, str);
+				editor.commit();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case R.id.cb_yibao:
+			try {
+				Toast.makeText(PaySettingActivity.this, "易宝：" + isChecked,
+						Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				editor.putString(KEY_YIBAO, str);
+				editor.commit();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		default:
+			break;
 		}
-		return super.onOptionsItemSelected(item);
 	}
 }
