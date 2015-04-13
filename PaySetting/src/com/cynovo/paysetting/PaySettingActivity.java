cc806c8065a8f0e94d908d3f5dc6e73ff5f6e27d
punan <punan2010@gmail.com>
2015-04-13 13:11:36 +0800

    add new feature

--- PaySetting/src/com/cynovo/paysetting/PaySettingActivity.java
+++ PaySetting/src/com/cynovo/paysetting/PaySettingActivity.java
@@ -5,13 +5,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
 
 public class PaySettingActivity extends Activity implements
-		OnCheckedChangeListener {
+		OnClickListener{
 	
 	public static final String TAG = "PaySetting";
 	public static final String PREFERENCENAME = "payconfig";
@@ -21,6 +22,11 @@ public class PaySettingActivity extends Activity implements
 	public static final String KEY_ZHIFUBAO = "zhifubao";
 	public static final String KEY_WEIXIN = "weixin";
 	public static final String KEY_YIBAO = "yibao";
+	
+	private CardView card_yinlian;
+	private CardView card_zhifubao;
+	private CardView card_weixin;
+	private CardView card_yibao;
 	
 	private CheckBox cb_yinlian;
 	private CheckBox cb_zhifubao;
@@ -48,28 +54,44 @@ public class PaySettingActivity extends Activity implements
 
 		editor.commit();
 
+		card_yinlian = (CardView)findViewById(R.id.card_yinlian);
+		card_zhifubao = (CardView)findViewById(R.id.card_zhifubao);
+		card_weixin = (CardView)findViewById(R.id.card_weixin);
+		card_yibao = (CardView)findViewById(R.id.card_yibao);
+		
+		card_yinlian.setOnClickListener(this);
+		card_zhifubao.setOnClickListener(this);
+		card_weixin.setOnClickListener(this);
+		card_yibao.setOnClickListener(this);
+		
 		cb_yinlian = (CheckBox) findViewById(R.id.cb_yinlian);
 		cb_zhifubao = (CheckBox) findViewById(R.id.cb_zhifubao);
 		cb_weixin = (CheckBox) findViewById(R.id.cb_weixin);
 		cb_yibao = (CheckBox) findViewById(R.id.cb_yibao);
 
-		cb_yinlian.setOnCheckedChangeListener(this);
-		cb_zhifubao.setOnCheckedChangeListener(this);
-		cb_weixin.setOnCheckedChangeListener(this);
-		cb_yibao.setOnCheckedChangeListener(this);
-
+		//cb_yinlian.setOnCheckedChangeListener(this);
+		//cb_zhifubao.setOnCheckedChangeListener(this);
+		//cb_weixin.setOnCheckedChangeListener(this);
+		//cb_yibao.setOnCheckedChangeListener(this);
+		
 	}
-
+	
 	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		int id = buttonView.getId();
-		switch (id) {
-		case R.id.cb_yinlian:
+		int id = v.getId();
+		switch(id){
+		case R.id.card_yinlian :
+			if(cb_yinlian.isChecked()){
+				cb_yinlian.setChecked(false);
+			}else{
+				cb_yinlian.setChecked(true);
+			}
+			
 			try {
-				Toast.makeText(PaySettingActivity.this, "银联：" + isChecked,
-						Toast.LENGTH_SHORT).show();
-				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				Toast.makeText(PaySettingActivity.this, "银联：" + cb_yinlian.isChecked(),
+					Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(cb_yinlian.isChecked()));
 				editor.putString(KEY_YINLIAN, str);
 				editor.commit();
 			} catch (Exception e) {
@@ -77,11 +99,18 @@ public class PaySettingActivity extends Activity implements
 				e.printStackTrace();
 			}
 			break;
-		case R.id.cb_zhifubao:
+			
+		case R.id.card_zhifubao:
+			if(cb_zhifubao.isChecked()){
+				cb_zhifubao.setChecked(false);
+			}else{
+				cb_zhifubao.setChecked(true);
+			}
+			
 			try {
-				Toast.makeText(PaySettingActivity.this, "支付宝：" + isChecked,
-						Toast.LENGTH_SHORT).show();
-				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				Toast.makeText(PaySettingActivity.this, "支付宝：" + cb_zhifubao.isChecked(),
+					Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(cb_zhifubao.isChecked()));
 				editor.putString(KEY_ZHIFUBAO, str);
 				editor.commit();
 			} catch (Exception e) {
@@ -89,11 +118,17 @@ public class PaySettingActivity extends Activity implements
 				e.printStackTrace();
 			}
 			break;
-		case R.id.cb_weixin:
+			
+		case R.id.card_weixin:
+			if(cb_weixin.isChecked()){
+				cb_weixin.setChecked(false);
+			}else{
+				cb_weixin.setChecked(true);
+			}
 			try {
-				Toast.makeText(PaySettingActivity.this, "微信：" + isChecked,
-						Toast.LENGTH_SHORT).show();
-				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				Toast.makeText(PaySettingActivity.this, "微信：" + cb_weixin.isChecked(),
+					Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(cb_weixin.isChecked()));
 				editor.putString(KEY_WEIXIN, str);
 				editor.commit();
 			} catch (Exception e) {
@@ -101,11 +136,17 @@ public class PaySettingActivity extends Activity implements
 				e.printStackTrace();
 			}
 			break;
-		case R.id.cb_yibao:
+			
+		case R.id.card_yibao:
+			if(cb_yibao.isChecked()){
+				cb_yibao.setChecked(false);
+			}else{
+				cb_yibao.setChecked(true);
+			}
 			try {
-				Toast.makeText(PaySettingActivity.this, "易宝：" + isChecked,
-						Toast.LENGTH_SHORT).show();
-				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(isChecked));
+				Toast.makeText(PaySettingActivity.this, "易宝：" + cb_yibao.isChecked(),
+					Toast.LENGTH_SHORT).show();
+				String str = AESCrypto.encrypt(PASSWORD, String.valueOf(cb_yibao.isChecked()));
 				editor.putString(KEY_YIBAO, str);
 				editor.commit();
 			} catch (Exception e) {
@@ -113,8 +154,7 @@ public class PaySettingActivity extends Activity implements
 				e.printStackTrace();
 			}
 			break;
-		default:
-			break;
 		}
 	}
+
 }
--- PaySetting/src/com/cynovo/paysetting/PaySettingProvider.java
+++ PaySetting/src/com/cynovo/paysetting/PaySettingProvider.java
@@ -2,11 +2,18 @@ package com.cynovo.paysetting;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
 
 public class PaySettingProvider extends ContentProvider {
-
+	
+	public static final String TAG = "PaySettingProvider";
+	
+	private SharedPreferences PaySettingPreference;
+	
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		// TODO Auto-generated method stub
@@ -28,14 +35,26 @@ public class PaySettingProvider extends ContentProvider {
 	@Override
 	public boolean onCreate() {
 		// TODO Auto-generated method stub
-		return false;
+		PaySettingPreference = this.getContext().getSharedPreferences(PaySettingActivity.PREFERENCENAME, Context.MODE_PRIVATE);
+		
+		return true;
 	}
 
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
 		// TODO Auto-generated method stub
-		return null;
+		String[] table = new String[]{"key","value"};
+		
+		MatrixCursor c = new MatrixCursor(table);
+		
+		c.addRow(new Object[]{"yinlian",PaySettingPreference.getString("yinlian", "false")});
+		c.addRow(new Object[]{"yinlian",PaySettingPreference.getString("yinlian", "false")});
+		c.addRow(new Object[]{"yinlian",PaySettingPreference.getString("yinlian", "false")});
+		c.addRow(new Object[]{"yinlian",PaySettingPreference.getString("yinlian", "false")});
+		
+		return c;
+		
 	}
 
 	@Override
